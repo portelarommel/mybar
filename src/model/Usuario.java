c2cc95323f097b8897b3b3eb46f11510bb65c344
portelarommel <portelarommel@gmail.com>
2026-06-10 11:01:15 -0300

    Primeiro commit

---
+++ src/model/Usuario.java
@@ -0,0 +1,18 @@
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    private String codigo;
+
+
+}
