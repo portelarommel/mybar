52e13fd2f19262741ea71fb16d129c03e5658c32
portelarommel <portelarommel@gmail.com>
2026-06-23 13:05:29 -0300

    Adicionei uma dependencia que analisa CPF

--- src/main/java/br/com/trabalhopoo/mybar/model/Cliente.java
+++ src/main/java/br/com/trabalhopoo/mybar/model/Cliente.java
@@ -1,9 +1,13 @@
 package br.com.trabalhopoo.mybar.model;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+
+import org.hibernate.validator.constraints.br.CPF;
+
 import  br.com.trabalhopoo.mybar.enums.Sexo;
 
 @Getter
@@ -13,15 +17,17 @@ import  br.com.trabalhopoo.mybar.enums.Sexo;
 @Entity
 @Table(name = "clientes")
 public class Cliente {
+    @NotBlank
     private String nome;
     @Id
     @Column(unique = true)
+    @NotBlank @CPF
     private String cpf;
+
     private String celular;
     @Enumerated(EnumType.STRING)
     private Sexo sexo;
     @OneToMany
     private Conta conta;
-
 
 }
--- src/main/java/br/com/trabalhopoo/mybar/model/Configuracao.java
+++ src/main/java/br/com/trabalhopoo/mybar/model/Configuracao.java
@@ -8,7 +8,9 @@ import br.com.trabalhopoo.mybar.enums.ModoOperacao;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,6 +20,9 @@ import lombok.Setter;
 @Entity
 @Table(name = "configuracao")
 public class Configuracao {
+    @Id
+    @NotNull
+    private Long id = 1L;
     private BigDecimal valorIngressoMasc;
     private BigDecimal valorIngressoFemin;
     private LocalDate data;
