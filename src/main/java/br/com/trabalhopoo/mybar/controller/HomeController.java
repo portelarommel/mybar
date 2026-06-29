2456afb41a6bebe351edc0af74a5be5f76e40061
portelarommel <portelarommel@gmail.com>
2026-06-29 13:15:56 -0300

    Adicionado praticamente todas os htmls que usaremos. Homecontroller é apenas um protótipo para testar as páginas

--- src/main/java/br/com/trabalhopoo/mybar/controller/HomeController.java
+++ src/main/java/br/com/trabalhopoo/mybar/controller/HomeController.java
@@ -19,14 +19,100 @@ public class HomeController {
     {
         return "conta/edicaoDeConta";
     }
-    @GetMapping("/conta/registroDeConta")
+    @GetMapping("/conta/registroDeItemDeConta")
+    public String registroDeItemDeConta()
+    {
+        return "conta/registroDeItemConta";
+    }
+    @GetMapping("/conta/registro")
     public String registroDeConta()
     {
         return "conta/registroDeConta";
+    }
+    @GetMapping("/conta/visualizacao")
+    public String visualizacaoDeConta()
+    {
+        return "conta/visualizacaoDeConta";
     }
     @GetMapping("/conta/fechamentoDeConta")
     public String fechamentoDeConta()
     {
         return "conta/fechamentoDeConta";
+    }
+    @GetMapping("/itemCardapio")
+    public String gestaoDeItemCardapio()
+    {
+        return "itemCardapio/gestaoDeItemCardapio";
+    }
+    @GetMapping("/itemCardapio/registro")
+    public String registroDeItemCardapio()
+    {
+        return "itemCardapio/registroDeItemCardapio";
+    }
+    @GetMapping("/itemCardapio/visualizar")
+    public String visualizacaoDeItemCardapio()
+    {
+        return "itemCardapio/visualizacaoDeItemCardapio";
+
+    }
+    @GetMapping("/tipoItem")
+    public String gestaoDeTipoDeItem()
+    {
+        return "tipoDeItem/gestaoTipoDeItem";
+    }
+    @GetMapping("/tipoItem/registro")
+    public String registroDeTipoDeItem()
+    {
+        return "tipoDeItem/registroDeTipoDeItem";
+    }
+    @GetMapping("/tipoItem/edicao")
+    public String edicaoDeTipoDeItem()
+    {
+        return "tipoDeItem/edicaoDeTipoDeItem";
+    }
+    @GetMapping("/tipoItem/visualizacao")
+    public String visualizacaoDeTipoDeItem()
+    {
+        return "tipoDeItem/visualizacaoDeTipoDeItem";
+    }
+    @GetMapping("/controleDeEntrega")
+    public String gestaoDeControleDeEntrega()
+    {
+        return "gestaoDeControleDeEntrega";
+    }
+    @GetMapping("/configuracaoDeSistema")
+    public String gestaoDeConfiguracao()
+    {
+        return "gestaoDeConfiguracao";
+    }
+    @GetMapping("/usuarios")
+    public String gestaoDeUsuario()
+    {
+        return "usuario/gestaoDeUsuario";
+    }
+    @GetMapping("/usuarios/edicao")
+    public String edicaoDeUsuario()
+    {
+        return "usuario/edicaoDeUsuario";
+    }
+    @GetMapping("/usuarios/registro")
+    public String registroDeUsuario()
+    {
+        return "usuario/registroDeUsuario";
+    }
+    @GetMapping("/usuarios/visualizacao")
+    public String visualizacaoDeUsuario()
+    {
+        return "usuario/visualizacaoDeUsuario";
+    }
+    @GetMapping("/controleDeCozinha")
+    public String controleDeCozinha()
+    {
+        return "gestaoDeControleDeCozinha";
+    }
+    @GetMapping("/dashboard")
+    public String dashboard()
+    {
+        return "dashboard";
     }
 }
